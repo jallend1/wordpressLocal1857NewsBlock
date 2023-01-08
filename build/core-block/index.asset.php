0678294717e53ae431e99a37bc3050fa492f776a
Jason Allen <39307371+jallend1@users.noreply.github.com>
2023-01-08 13:02:35 -0800

    Removes red border around block in editor

--- build/core-block/index.asset.php
+++ build/core-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cfe96269f698a98a64e5');
+<?php return array('dependencies' => array('wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '21c6c831f03afecb57ad');
