a63369268e0a9c2ddb4b2136bc0fa4483fdba95a
Jason Allen <39307371+jallend1@users.noreply.github.com>
2023-01-08 12:41:47 -0800

    Plugin now officially renders the five latest news posts

--- build/core-block/render.php
+++ build/core-block/render.php
@@ -1,4 +1,4 @@
-<?php 
+<?php  
     $args = array(
         'post_type' => 'post',
         'posts_per_page' => 5,
@@ -6,47 +6,44 @@
         'order' => 'DESC'
     );
     $the_query = new WP_Query($args); 
-    ob_start();
-	?>
-        <main class="kcls-section">
-            <div class="kcls-recent-posts">
-                <?php if ( $the_query->have_posts() ) ?>
-                <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                    <div 
-                        class="<?php
-                            // Applies unique class to most recent post
-                            if($the_query->current_post === 0) echo 'kcls-most-recent-post'; 
-                            else echo('kcls-recent-post'); ?>">                
-                        <div class="kcls-news-details">
-                            <a href="<?php the_permalink(); ?>">
-                                <h3><?php the_title(); ?></h3>
-                            </a>
-                            <p class="kcls-recent-post-time"><?php the_date(); ?></p>    
-                        </div>
-                        <div 
-                            class="kcls-recent-post-expanded <?php if($the_query->current_post === 0) echo 'kcls-blurb-and-image';?>">
-                            <?php 
-                                // Shows excerpt if it is the most recent post
-                                if($the_query->current_post === 0) the_excerpt(); 
-                            ?> 
-                            <div class="kcls-news-image">
-                                <div class="kcls-recent-post-thumbnail">
-                                    <?php if ( has_post_thumbnail() ){
-                                        the_post_thumbnail('medium');
-                                    } 
-                                    // If no featured image, display the first image from the post or default logo
-                                    else {
-                                        $kcls_post_img = kcls_get_post_image();
-                                        echo '<img src="' . $kcls_post_img . '" alt="' . get_the_title() . '">';
-                                    } ?> 
-                                </div>
-                            </div>
+?>
+<main class="kcls-section">
+    <div class="kcls-recent-posts">
+        <?php if ( $the_query->have_posts() ) ?>
+        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <div 
+                class="<?php
+                    // Applies unique class to most recent post
+                    if($the_query->current_post === 0) echo 'kcls-most-recent-post'; 
+                    else echo('kcls-recent-post'); ?>">                
+                <div class="kcls-news-details">
+                    <a href="<?php the_permalink(); ?>">
+                        <h3><?php the_title(); ?></h3>
+                    </a>
+                    <p class="kcls-recent-post-time"><?php the_date(); ?></p>    
+                </div>
+                <div 
+                    class="kcls-recent-post-expanded <?php if($the_query->current_post === 0) echo 'kcls-blurb-and-image';?>">
+                    <?php 
+                        // Shows excerpt if it is the most recent post
+                        if($the_query->current_post === 0) the_excerpt(); 
+                    ?> 
+                    <div class="kcls-news-image">
+                        <div class="kcls-recent-post-thumbnail">
+                            <?php if ( has_post_thumbnail() ){
+                                the_post_thumbnail('medium');
+                            } 
+                            // If no featured image, display the first image from the post or default logo
+                            else {
+                                $kcls_post_img = kcls_get_post_image();
+                                echo '<img src="' . $kcls_post_img . '" alt="' . get_the_title() . '">';
+                            } ?> 
                         </div>
                     </div>
-                <?php endwhile; ?>
+                </div>
             </div>
-        </main>
-
-	<?php
-	wp_reset_postdata();
-    return ob_get_clean();
+        <?php endwhile; ?>
+    </div>
+</main>
+<?php
+    wp_reset_postdata();
